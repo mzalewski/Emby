ff5fa376d50a8944af643c6b3bd61b4c9f95d898
mzalewski <matthew@minion.co.nz>
2016-06-28 17:34:12 +1200

    Missed files

---
+++ MediaBrowser.WebDashboard/dashboard-ui/voice/voiceprocessor.js
@@ -0,0 +1,73 @@
+﻿define(function () {
+
+    var commandgroups;
+
+    function getCommandGroups() {
+
+        if (commandgroups) {
+            return Promise.resolve(commandgroups);
+        }
+
+        return new Promise(function (resolve, reject) {
+
+            var file = "grammar";
+            //if (language && language.length > 0)
+            //    file = language;
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', "voice/grammar/" + file + ".json", true);
+
+            xhr.onload = function (e) {
+
+                commandgroups = JSON.parse(this.response);
+                resolve(commandgroups);
+            }
+
+            xhr.onerror = reject;
+
+            xhr.send();
+        });
+    }
+    /// <summary> Process the transcript described by text. </summary>
+    /// <param name="text"> The text. </param>
+    /// <returns> . </returns>
+    function processTranscript(text) {
+        return new Promise(function(resolve, reject) {
+            if (text) {
+                require(['voice/voicecommands.js', 'voice/grammarprocessor.js'],
+                    function(voicecommands, grammarprocessor) {
+
+                        var processor = grammarprocessor(commandgroups, text);
+                        if (processor && processor.command) {
+                            console.log("Command from Grammar Processor", processor);
+                            voicecommands(processor)
+                                .then(function(result) {
+                                    console.log("Result of executed command", result);
+                                    if (result.item.actionid === 'show' && result.item.sourceid === 'group') {
+                                        reject({ error: "group", item: result.item, groupName: result.name });
+                                    } else {
+                                        resolve({ item: result.item });
+                                    }
+                                })
+                                .catch(function() {
+                                    reject({ error: "unrecognized-command", text: text });
+                                });
+                        } else {
+                            reject({ error: "unrecognized-command", text: text });
+                        }
+
+                    });
+
+            } else {
+                reject({ error: "empty" });
+            }
+        });
+    }
+
+    /// <summary> An enum constant representing the window. voice input manager option. </summary>
+    return {
+        processTranscript: processTranscript,
+        getCommandGroups: getCommandGroups
+    };
+
+});
---
+++ MediaBrowser.WebDashboard/dashboard-ui/voice/voicereceiver.js
@@ -0,0 +1,65 @@
+﻿define(function () {
+    var currentRecognition = null;
+   
+
+    /// <summary> Starts listening for voice commands </summary>
+    /// <returns> . </returns>
+    function listenForCommand( lang ) {
+        return new Promise(function(resolve, reject) {
+            destroyCurrentRecognition();
+            var recognition = new (window.SpeechRecognition ||
+                window.webkitSpeechRecognition ||
+                window.mozSpeechRecognition ||
+                window.oSpeechRecognition ||
+                window.msSpeechRecognition)();
+            recognition.lang = lang;
+            //recognition.continuous = true;
+            //recognition.interimResults = true;
+
+            recognition.onresult = function (event) {
+                console.log(event);
+                if (event.results.length > 0) {
+                    var resultInput = event.results[0][0].transcript || '';
+                    resolve(resultInput);
+                }
+            };
+
+            recognition.onerror = function() {
+                reject({ error: event.error, message: event.message });
+            };
+
+            recognition.onnomatch = function() {
+                reject({ error: "no-match" });
+            };
+
+            recognition.start();
+            currentRecognition = recognition;
+        });
+    }
+
+    /// <summary> Destroys the current recognition. </summary>
+    /// <returns> . </returns>
+    function destroyCurrentRecognition() {
+
+        var recognition = currentRecognition;
+        if (recognition) {
+            recognition.abort();
+            currentRecognition = null;
+        }
+    }
+
+    /// <summary> Cancel listener. </summary>
+    /// <returns> . </returns>
+    function cancelListener() {
+
+        destroyCurrentRecognition();
+        
+    }
+
+    /// <summary> An enum constant representing the window. voice input manager option. </summary>
+    return {
+        listenForCommand: listenForCommand,
+        cancel: cancelListener
+    };
+
+});
